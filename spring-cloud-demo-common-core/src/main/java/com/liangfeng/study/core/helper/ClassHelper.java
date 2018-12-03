dc556f925c41b5190c2168730fa83673b089f0a7
DESKTOP-BMHS4TU\61661 <616611974@qq.com>
2018-12-03 16:14:07 +0800

    1.增加了相应的工具类

--- spring-cloud-demo-common-core/src/main/java/com/liangfeng/study/core/helper/ClassHelper.java
+++ spring-cloud-demo-common-core/src/main/java/com/liangfeng/study/core/helper/ClassHelper.java
@@ -1,6 +1,11 @@
 package com.liangfeng.study.core.helper;
 
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Title: ClassHelper.java
  * @Description: 类帮助类，获取类的一些信息
@@ -15,32 +20,25 @@ public class ClassHelper {
 	 * 构造私有化
 	 */
 	private ClassHelper(){}
-	
+
 	/**
-	 * 获取对象实例的类名称
-	 * @param instance
+	 * 获取类的方法集合
+	 *
+	 * @param clazz
+	 * @param isContainParent
 	 * @return
 	 */
-	public static String getClassName(Object instance) {
-        String ret = "";
-        Class clazz = getClazz(instance);
-        if (null != clazz) {
-            ret = clazz.getName();
-        }
-        return ret;
-    }
-	
-	private static Class<?> getClazz(Object instance) {
-		Class clazz = null;
-		if (null != instance) {
-			clazz = instance.getClass();
-			if ((clazz != null) && clazz.getName().contains("$$")) {
-				Class<?> superClass = clazz.getSuperclass();
-				if ((superClass != null) && !Object.class.equals(superClass)) {
-					return superClass;
-				}
+	private  Method[] getMethods(Class clazz, boolean isContainParent) {
+		List<Method> methods = new ArrayList<>();
+		Class tempClass = clazz;
+		if (isContainParent) {
+			while (tempClass != null) {
+				methods.addAll(Arrays.asList(tempClass.getMethods()));
+				tempClass = tempClass.getSuperclass();
 			}
+		} else {
+			methods.addAll(Arrays.asList(tempClass.getMethods()));
 		}
-		return clazz;
+		return methods.toArray(new Method[]{});
 	}
 }
